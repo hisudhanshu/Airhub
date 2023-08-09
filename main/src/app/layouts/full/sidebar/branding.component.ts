c40a47edba58b246a8f07a4e2ef39563316f057e
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-08-09 11:04:27 +0530

    This is first commit of Airhub admin panel

---
+++ main/src/app/layouts/full/sidebar/branding.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branding',
+  template: `
+    <div class="branding">
+      <a href="/">
+        <img
+          src="https://www.airhubapp.com/assets/images/logoair.png" style="height: 40px;"
+          class="align-middle m-2"
+          alt="logo"
+        />
+      </a>
+    </div>
+  `,
+})
+export class BrandingComponent {
+  constructor() {}
+}
